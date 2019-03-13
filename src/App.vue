e410349889944a9b6042d5071c1bd2dfb33941e2
Sean Dvir <seandvir12@gmail.com>
2019-03-13 16:06:32 +0200

    added repo link

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,51 @@
 <template>
-  <v-app>
-    <v-toolbar app dark>
-      <v-toolbar-title class="headline">
-        <span>Note-Art JS Demo</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-              flat
-              href="https://github.com/Seanitzel/note-art"
-              target="_blank"
-      >
-        <span class="display-1" style="color: brown;">Repo</span>
-      </v-btn>
-      <v-btn
-        flat
-        href="https://seanitzel.github.io/Note-Art/index.html"
-        target="_blank"
-      >
-        <span class="display-1" style="color: brown;">Docs</span>
-      </v-btn>
-    </v-toolbar>
+    <v-app>
+        <v-toolbar app dark>
+            <v-toolbar-title class="headline">
+                <span>Note-Art JS Demo</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn
+                    flat
+                    href="https://github.com/Seanitzel/note-art"
+                    target="_blank"
+            >
+                <span class="display-1" style="color: brown;">Repo</span>
+            </v-btn>
+            <v-btn
+                    flat
+                    href="https://seanitzel.github.io/Note-Art/index.html"
+                    target="_blank"
+            >
+                <span class="display-1" style="color: brown;">Docs</span>
+            </v-btn>
+            <v-btn
+                    flat
+                    href="https://github.com/Seanitzel/Note-Art-Demo"
+                    target="_blank"
+            >
+                <span class="display-1" style="color: brown;">Source</span>
+            </v-btn>
+        </v-toolbar>
 
-    <v-content>
-      <demo/>
-    </v-content>
-  </v-app>
+        <v-content>
+            <demo/>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import demo from './components/demo'
+    import demo from './components/demo'
 
-export default {
-  name: 'App',
-  components: {
-    demo
-  },
-  data () {
-    return {
-      //
+    export default {
+        name:       'App',
+        components: {
+            demo,
+        },
+        data() {
+            return {
+                //
+            }
+        },
     }
-  }
-}
 </script>
